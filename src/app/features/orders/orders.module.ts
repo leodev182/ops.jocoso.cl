35ae0f24e32763bc980c7788163bd0383acc0c0d
nonedev <leoartdev@gmail.com>
2026-04-25 01:19:10 -0400

    feat(orders): add orders list and order detail pages

--- src/app/features/orders/orders.module.ts
+++ src/app/features/orders/orders.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { OrdersRoutingModule } from './orders-routing.module';
-
+import { OrdersListPageComponent } from './pages/orders-list-page/orders-list-page.component';
+import { OrderDetailPageComponent } from './pages/order-detail-page/order-detail-page.component';
+import { OrdersTableComponent } from './components/orders-table/orders-table.component';
 
 @NgModule({
-  declarations: [],
   imports: [
     CommonModule,
-    OrdersRoutingModule
-  ]
+    OrdersRoutingModule,
+    OrdersListPageComponent,
+    OrderDetailPageComponent,
+    OrdersTableComponent,
+  ],
 })
-export class OrdersModule { }
+export class OrdersModule {}
