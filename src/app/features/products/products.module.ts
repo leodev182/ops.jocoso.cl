05b033d93f256f0ed8ab2f9f56f312571829b9e0
nonedev <leoartdev@gmail.com>
2026-04-25 01:17:41 -0400

    feat(products): add product list, detail, variant form and ML sync

--- src/app/features/products/products.module.ts
+++ src/app/features/products/products.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { ProductsRoutingModule } from './products-routing.module';
-
+import { ProductsListPageComponent } from './pages/products-list-page/products-list-page.component';
+import { ProductDetailPageComponent } from './pages/product-detail-page/product-detail-page.component';
+import { ProductTableComponent } from './components/product-table/product-table.component';
+import { VariantFormComponent } from './components/variant-form/variant-form.component';
 
 @NgModule({
-  declarations: [],
   imports: [
     CommonModule,
-    ProductsRoutingModule
-  ]
+    ProductsRoutingModule,
+    ProductsListPageComponent,
+    ProductDetailPageComponent,
+    ProductTableComponent,
+    VariantFormComponent,
+  ],
 })
-export class ProductsModule { }
+export class ProductsModule {}
