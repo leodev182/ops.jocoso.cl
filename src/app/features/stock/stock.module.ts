02f1ef4e5083d2ef6a20e41c2ae30355187fe199
nonedev <leoartdev@gmail.com>
2026-04-25 01:18:37 -0400

    feat(stock): add stock level view, movements history and manual adjustment

--- src/app/features/stock/stock.module.ts
+++ src/app/features/stock/stock.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { StockRoutingModule } from './stock-routing.module';
-
+import { StockPageComponent } from './pages/stock-page/stock-page.component';
+import { MovementsTableComponent } from './components/movements-table/movements-table.component';
 
 @NgModule({
-  declarations: [],
   imports: [
     CommonModule,
-    StockRoutingModule
-  ]
+    StockRoutingModule,
+    StockPageComponent,
+    MovementsTableComponent,
+  ],
 })
-export class StockModule { }
+export class StockModule {}
